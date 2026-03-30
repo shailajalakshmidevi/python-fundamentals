420ea7f4a4fa6c9c041f0560efe347666b1154c9
shailajalakshmidevi <160513009+shailajalakshmidevi@users.noreply.github.com>
2026-03-30 20:45:28 +0530

    added CLI error handling and file input support

--- projects/text_analyzer/main.py
+++ projects/text_analyzer/main.py
@@ -1,30 +1,34 @@
 from analyser import *
 import sys
 
-if len(sys.argv) <2:
-    
-     print("error")
-     exit()
-else:    
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <filename>")
+    exit()
 
-     filename = sys.argv[1]
-     with open("sample.txt") as file:
-          text = file.read()
-     print("Text:", text)
-     print(filename)
+filename = sys.argv[1]
+
+try:
+    with open(filename, "r") as file:
+        text = file.read()
+except FileNotFoundError:
+    print("Error: File not found")
+    exit()
 
 
-vowel_count, consonant_count, vowel_list, consonant_list, char_count, total_chars, words, words_length = analyser(text)
+print("Text:", text)
 
-print("Total vowels =", vowel_count)
+(vowel_count, consonant_count, vowel_list, consonant_list,
+ char_count, total_chars, words, words_length) = analyser(text)
+
+print("\nTotal vowels =", vowel_count)
 print("Vowels are =", vowel_list)
 
-print("Total consonants =", consonant_count)
+print("\nTotal consonants =", consonant_count)
 print("Consonants are =", consonant_list)
 
-print("Total characters =", total_chars)
+print("\nTotal characters =", total_chars)
 
-print("Words in text =", words)
+print("\nWords in text =", words)
 print("Total words =", words_length)
 
-print("Character frequency =", char_count)
+print("\nCharacter frequency =", char_count)
