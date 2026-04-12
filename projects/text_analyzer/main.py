3aa6b32cda0526d18998621859f600f2bd6c5a5a
shailajalakshmidevi <160513009+shailajalakshmidevi@users.noreply.github.com>
2026-04-12 10:54:02 +0530

    implemented logging using Python's logging module to track execution flow and handle errors efficiently

--- projects/text_analyzer/main.py
+++ projects/text_analyzer/main.py
@@ -1,34 +1,44 @@
-from analyser import *
 import sys
+import logging
+from analyser import analyser
+# 🔹 Logging setup
+logging.basicConfig(
+    filename='app.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
 
 if len(sys.argv) < 2:
     print("Usage: python3 main.py <filename>")
+    logging.warning("No filename provided")
     exit()
 
 filename = sys.argv[1]
 
 try:
+    logging.info(f"Opening file: {filename}")
+
     with open(filename, "r") as file:
         text = file.read()
+
+    print("Text:", text)
+    logging.info("File read successfully")
+
+    v_count, c_count, v_list, c_list, freq, total_chars, words, total_words = analyser(text)
+    print("\nTotal vowels =", v_count)
+    print("Vowels =", v_list)
+
+    print("\nTotal consonants =", c_count)
+    print("Consonants =", c_list)
+
+    print("\nTotal words =", total_words)
+    print("Words =", words)
+
+    print("\nCharacter frequency =", freq)
+
+    logging.info("Analysis completed successfully")
+
 except FileNotFoundError:
     print("Error: File not found")
-    exit()
-
-
-print("Text:", text)
-
-(vowel_count, consonant_count, vowel_list, consonant_list,
- char_count, total_chars, words, words_length) = analyser(text)
-
-print("\nTotal vowels =", vowel_count)
-print("Vowels are =", vowel_list)
-
-print("\nTotal consonants =", consonant_count)
-print("Consonants are =", consonant_list)
-
-print("\nTotal characters =", total_chars)
-
-print("\nWords in text =", words)
-print("Total words =", words_length)
-
-print("\nCharacter frequency =", char_count)
+    logging.error(f"File not found: {filename}")
